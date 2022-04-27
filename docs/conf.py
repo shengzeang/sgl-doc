cbbb9be981055bb78027d73b591f38b3e6a3d298
shengzeang <72069497+shengzeang@users.noreply.github.com>
2022-04-27 15:57:43 +0800

    create hierarchy

---
+++ docs/conf.py
@@ -0,0 +1,62 @@
+import sphinx_rtd_theme
+from recommonmark.parser import CommonMarkParser
+from recommonmark.transform import AutoStructify
+
+
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'SGL'
+copyright = '2022, DAIR @PKU'
+author = 'DAIR @PKU'
+
+release = 'beta'
+version = '0.1.2'
+
+# -- General configuration
+extensions = ['recommonmark',
+              'sphinx.ext.autodoc',
+              'sphinx.ext.doctest',
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.todo',
+              'sphinx.ext.coverage',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.napoleon',
+              'sphinx_markdown_tables']
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+language = 'en'
+master_doc = 'index'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
+github_doc_root = 'https://github.com/rtfd/recommonmark/tree/master/docs/'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
+
+# app setup hook
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+        'url_resolver': lambda url: github_doc_root + url,
+        'auto_toc_tree_section': 'Contents',
+        'enable_math': False,
+        'enable_inline_math': False,
+        'enable_eval_rst': True
+    }, True)
+    app.add_transform(AutoStructify)
